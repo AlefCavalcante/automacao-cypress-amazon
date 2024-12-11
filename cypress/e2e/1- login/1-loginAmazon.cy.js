29956dc927eb79480c85275974e5517626e89400
AlefCavalcante <alefcavalcante493@gmail.com>
2024-12-11 13:44:49 -0300

    Update 1-loginAmazon.cy.js

--- cypress/e2e/1- login/1-loginAmazon.cy.js
+++ cypress/e2e/1- login/1-loginAmazon.cy.js
@@ -2,14 +2,12 @@
 
 describe('testes funcionais de login', () => {
     it('deve realizar o login com sucesso', () => {
-        cy.wait(10000);
         cy.visit('https://www.amazon.com.br/ref=nav_logo');
-
-        cy.wait(10000);
+        
         // Aguarda o botão de login estar visível e clica
         cy.get('#nav-link-accountList-nav-line-1', { timeout: 15000 })
-        .should('be.visible') // Garante que o botão é visível
-        .click();
+            .should('be.visible') // Garante que o botão é visível
+            .click();
         
         // Insere o email e avança
         cy.get('#ap_email').type("jailsoncavalcante493@gmail.com");
@@ -20,6 +18,8 @@ describe('testes funcionais de login', () => {
         cy.get('#signInSubmit').click();
         
         // Aguarda o texto de saudação aparecer
-        cy.get('#nav-link-accountList', { timeout: 10000 }).should('contain', 'Olá, Alef');
+        cy.get('#nav-link-accountList', { timeout: 15000 })
+            .should('contain', 'Olá, Alef') // Confirma se a saudação foi exibida
+            .and('be.visible'); // Garante que o elemento está visível
     });
 });
